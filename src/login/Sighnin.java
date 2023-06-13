f6d1d91942834d27d66fc31f8f47666139f3dd63
unknown <imkapil1997@gmail.com>
2023-06-13 16:46:31 +0530

    New Code

---
+++ src/login/Sighnin.java
@@ -0,0 +1,17 @@
+package login;
+
+public class Sighnin {
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Automation Testing");
+		Sighnin a=new Sighnin();
+		a.loginid();
+	}
+	
+	public void loginid()
+	{
+		System.out.println("Login ID");
+	}
+
+}
---
+++ src/product/Item.java
@@ -0,0 +1,26 @@
+package product;
+
+public class Item {
+	String pm="Modi";
+public static void main(String[] args)
+{
+ String name="kapil";
+ System.out.println(name);
+ Item a=new Item();
+ a.testing();
+ a.testing2();
+}
+
+public void testing()
+{
+	int age=20;
+System.out.println(age);
+}
+
+public void testing2()
+{
+	int ids=23;
+System.out.println(ids);
+System.out.println(pm);
+}
+}
